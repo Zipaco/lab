03103b90f9e9d3834ac4fa7ebf6ece6407edc0e8
Zipaco <166917275+Zipaco@users.noreply.github.com>
2024-09-14 15:07:51 +0700

    Add files via upload

---
+++ YearDays/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("YearDays")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("YearDays")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("34836701-94b4-4d13-8545-f8cc5f6ddfd7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ YearDays/YearDaysTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace YearDays
+{
+    [TestClass]
+    public class YearDaysTest
+    {
+        [TestMethod]
+        public void YearDaysValidDataTest()
+        {
+            var yearDays = new MathTaskClassLibrary.Class1.YearDays1();
+            Assert.AreEqual(365, yearDays.DaysInYear(2021));
+            Assert.AreEqual(366, yearDays.DaysInYear(2020));
+            Assert.AreEqual(365, yearDays.DaysInYear(1900));
+            Assert.AreEqual(366, yearDays.DaysInYear(2000));
+        }
+
+        [TestMethod]
+        public void YearDaysInvalidDataTest()
+        {
+            var yearDays = new MathTaskClassLibrary.Class1.YearDays1();
+            Assert.ThrowsException<ArgumentException>(() => yearDays.DaysInYear(0));
+            Assert.ThrowsException<ArgumentException>(() => yearDays.DaysInYear(-1));
+        }
+    }
+}
